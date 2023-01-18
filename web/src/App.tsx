6af4be32a7d12a5121aa6ad685a4fcda5f0b9080
Leonardo Augusto dos Santos Oliveira <Leonardoaugusto.soliveira@gmail.com>
2023-01-18 13:13:54 -0300

    initial

---
+++ web/src/App.tsx
@@ -0,0 +1,16 @@
+import { Habit } from "./components/Habit"
+import './styles/global.css'
+
+function App() {
+
+  return (
+    <div>
+      <Habit completed={1} />
+      <Habit completed={3} />
+      <Habit completed={5} />
+      <Habit completed={6} />
+    </div>
+  )
+}
+
+export default App
