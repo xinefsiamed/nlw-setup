1423fedd0a4785c27687426c2c1cad0be483a01d
Leonardo Augusto dos Santos Oliveira <Leonardoaugusto.soliveira@gmail.com>
2023-01-20 18:50:43 -0300

    Aula 2

---
+++ server/prisma/migrations/20230118163514_create_many_tables/migration.sql
@@ -0,0 +1,31 @@
+-- CreateTable
+CREATE TABLE "habit_week_days" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "week_day" TEXT NOT NULL,
+    "habit_id" TEXT NOT NULL,
+    CONSTRAINT "habit_week_days_habit_id_fkey" FOREIGN KEY ("habit_id") REFERENCES "habits" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "day" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "date" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "day_habit" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "day_id" TEXT NOT NULL,
+    "habit_id" TEXT NOT NULL,
+    CONSTRAINT "day_habit_day_id_fkey" FOREIGN KEY ("day_id") REFERENCES "day" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "day_habit_habit_id_fkey" FOREIGN KEY ("habit_id") REFERENCES "habits" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "habit_week_days_habit_id_week_day_key" ON "habit_week_days"("habit_id", "week_day");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "day_date_key" ON "day"("date");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "day_habit_day_id_habit_id_key" ON "day_habit"("day_id", "habit_id");
---
+++ web/src/components/SummaryTable.tsx
@@ -0,0 +1,40 @@
+import { generateDateFromYearBeginning } from "../utils/generate-date-from-year-beginning"
+import { HabitDay } from "./HabitDay"
+
+const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S']
+
+const summaryDates = generateDateFromYearBeginning()
+
+const minimumSummaryDatesSize = 18 * 7
+const amountDaysToFill = minimumSummaryDatesSize - summaryDates.length
+
+export function SummaryTable() {
+  return (
+    <div className="w-full flex">
+      <div className="grid grid-rows-7 grid-flow-row gap-3">
+        {weekDays.map((weekDay, i) => {
+          return (
+            <div
+              key={`${weekDay}-${i}`}
+              className="text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center"
+            >
+              {weekDay}
+            </div>
+          )
+        })}
+      </div>
+
+      <div className="grid grid-rows-7 grid-flow-col gap-3">
+        {summaryDates.map(date => {
+          return <HabitDay key={date.toString()} />
+        })}
+
+        {amountDaysToFill > 0 && Array.from({ length: amountDaysToFill }).map((_, i) => {
+          return (
+            <div key={i} className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"></div>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
